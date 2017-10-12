cacbedd55c6118f0b6cbd987ebe53296e4835d03
nbriannl <neilbrian.nl@gmail.com>
2017-10-12 20:30:00 +0800

    Revert "Merge pull request #50 from April0616/remark_command"
    This reverts commit 27c0a0bbef1043207eae386b682efb924afe2f14, reversing
    changes made to 2e1f77eb611cc71f332b29211c23e80499b9672c.

--- src/test/java/seedu/address/ui/PersonCardTest.java
+++ src/test/java/seedu/address/ui/PersonCardTest.java
@@ -35,7 +35,6 @@ public class PersonCardTest extends GuiUnitTest {
             personWithTags.setAddress(ALICE.getAddress());
             personWithTags.setEmail(ALICE.getEmail());
             personWithTags.setPhone(ALICE.getPhone());
-            personWithTags.setRemark(ALICE.getRemark());
             personWithTags.setTags(ALICE.getTags());
         });
         assertCardDisplay(personCard, personWithTags, 2);
