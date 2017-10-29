53bbca1dfd4f29d222ae4ffbb9346aa2514b3e39
LAPTOP-7KFM2KSP\User <594395096@qq.com>
2017-10-29 20:11:21 +0800

    update models

--- src/main/java/seedu/address/model/person/PhotoPath.java
+++ src/main/java/seedu/address/model/person/PhotoPath.java
@@ -2,20 +2,41 @@ package seedu.address.model.person;
 
 import static java.util.Objects.requireNonNull;
 
+import seedu.address.commons.exceptions.IllegalValueException;
+
 /**
  * Represents the path of a person's photo in the address book.
  */
 public class PhotoPath {
 
     public static final String MESSAGE_PHOTOPATH_CONSTRAINTS =
-            "Photo Path should be the absolute path in your PC";
+            "Photo Path should be the absolute path in your PC. It should be a string started with the name of " +
+                    "your disk, followed by several groups of backslash and string, like c:\\desktop\\happy.jpg";
 
-    public final String value;
 
-    public PhotoPath(String photoPath) {
+
+    public static final String PHOTOPATH_VALIDATION_REGEX = "([a-zA-Z]:)?(\\\\[a-zA-Z0-9_.-]+)+\\\\?";
+
+    public final String value;  //photo path
+
+    public PhotoPath(String photoPath) throws IllegalValueException {
         requireNonNull(photoPath);
+
+        //copy the photo file in PC to the picture folders.
+        if (!isValidPhotoPath(photoPath)) {
+            throw new IllegalValueException(MESSAGE_PHOTOPATH_CONSTRAINTS);
+        }
         this.value = photoPath;
     }
+
+
+    /**
+     * Returns if a given string is a valid photo path.
+     */
+    public static boolean isValidPhotoPath(String test) {
+        return test.matches(PHOTOPATH_VALIDATION_REGEX);
+    }
+
 
     @Override
     public String toString() {
